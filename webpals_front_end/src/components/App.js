d86f8114881ee642b778e8c008a39ce7ea5832f5
Jacob <jacob@perrysysinc.com>
2019-11-03 21:58:58 +0200

    some changes

--- webpals_front_end/src/components/App.js
+++ webpals_front_end/src/components/App.js
@@ -11,6 +11,7 @@ import AddStudent from './AddStudent';
 import AddGrade from './AddGrade';
 import DeleteStudent from './DeleteStudent';
 import EditStudent from './EditStudent';
+import StudentsTable from './StudentsTable';
 
 
 function App() {
@@ -44,11 +45,33 @@ export default App
 const HomePage = () => {
     return (
         <div className="text-center pt-5">
-            <h1>Welcome to the Student Grades Admin</h1>
-            <hr />
-            <p>
-                Laborum minim pariatur eiusmod consectetur veniam amet aute culpa eu veniam. Consectetur irure consectetur aute deserunt sit ipsum incididunt. Consectetur amet ullamco quis amet reprehenderit esse commodo proident in reprehenderit fugiat pariatur. Dolore ut in aute dolore et veniam. Non consequat incididunt proident velit voluptate ad reprehenderit cillum pariatur ullamco veniam enim pariatur.
-            </p>
+          <div className="row">
+              <div className="col-12">
+                  <h1 className="display-3">
+                      Welcome to the Student Grades Admin
+                  </h1>
+                  <hr />
+              </div>
+          </div>
+          <div className="row">
+              <div className="col">
+                <h1>
+                   Laboris incididunt nisi minim est elit. 
+                </h1>
+                <p className="text-left">
+                    Non adipisicing ullamco incididunt sit quis quis aliquip non exercitation. Tempor sint et do anim cupidatat enim laboris. Aute incididunt ipsum ullamco in. Mollit et mollit labore et pariatur sint nisi duis anim. Duis consequat reprehenderit cillum est Lorem sit cillum ipsum ad labore id laborum mollit. Labore eiusmod nulla dolor nulla sit aliquip quis laborum. Commodo esse et tempor laborum veniam enim elit laborum nostrud proident sint.    
+                </p> 
+              </div>
+              <div className="col">
+                  <h1>Proident nulla amet anim cupidatat consequat.</h1>
+                  <p className="text-left">
+                      Officia elit nisi adipisicing in ea ullamco aute Lorem qui consequat ad mollit. Duis aliqua non velit id enim ex voluptate. Mollit officia irure ipsum proident elit duis enim exercitation officia laborum labore quis.
+                  </p>
+                  <p className="text-left">
+                      Nisi quis et cillum qui ipsum id amet sunt. Ad sit culpa consequat minim eiusmod amet et nulla duis excepteur aute nulla dolore excepteur. Anim non magna cillum ipsum do minim fugiat fugiat reprehenderit ullamco non sit amet in. Culpa in enim aliqua pariatur do velit magna duis qui nulla dolor cupidatat cillum ea. Laboris amet nulla sit voluptate cillum est sunt ipsum labore esse mollit eiusmod officia exercitation. Ut eiusmod eiusmod incididunt deserunt exercitation cupidatat dolore laboris aute enim incididunt mollit ad. Ea magna eiusmod dolor incididunt laborum eu incididunt laboris ipsum in non adipisicing quis.
+                  </p>
+              </div>
+          </div>
         </div>
     )
 }
